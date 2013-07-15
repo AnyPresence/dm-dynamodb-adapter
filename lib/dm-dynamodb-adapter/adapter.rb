8dde9c0a0404ac4e12965b725ac2b011c34505fc
Nass <nasali@anypresence.com>
2013-07-15 14:48:56 -0400

    Added adapter method skeletons.

--- lib/dm-dynamodb-adapter/adapter.rb
+++ lib/dm-dynamodb-adapter/adapter.rb
@@ -10,6 +10,88 @@ module DataMapper
           @db = AWS::DynamoDB.new(:access_key_id => access_key_id,:secret_access_key => secret_access_key)
           DataMapper.logger.debug("Connected to #{@db}")
         end
+        
+        # Reads one or many resources from a datastore
+        #
+        # @example
+        #   adapter.read(query)  # => [ { 'name' => 'Dan Kubb' } ]
+        #
+        # Adapters provide specific implementation of this method
+        #
+        # @param [Query] query
+        #   the query to match resources in the datastore
+        #
+        # @return [Enumerable<Hash>]
+        #   an array of hashes to become resources
+        #
+        # @api semipublic
+        def read(query)
+          DataMapper.logger.debug("Read #{query.inspect} and its model is #{query.model.inspect}")
+          raise "Implement me!"
+        end
+        
+        # Persists one or many new resources
+        #
+        # @example
+        #   adapter.create(collection)  # => 1
+        #
+        # Adapters provide specific implementation of this method
+        #
+        # @param [Enumerable<Resource>] resources
+        #   The list of resources (model instances) to create
+        #
+        # @return [Integer]
+        #   The number of records that were actually saved into the data-store
+        #
+        # @api semipublic  
+        def create(resources)
+          resources.each do |resource|
+            model = resource.model
+            DataMapper.logger.debug("About to create #{model} using #{resource.attributes}")
+          end
+          raise "Implement me!"
+        end
+        
+        # Updates one or many existing resources
+        #
+        # @example
+        #   adapter.update(attributes, collection)  # => 1
+        #
+        # Adapters provide specific implementation of this method
+        #
+        # @param [Hash(Property => Object)] attributes
+        #   hash of attribute values to set, keyed by Property
+        # @param [Collection] collection
+        #   collection of records to be updated
+        #
+        # @return [Integer]
+        #   the number of records updated
+        #
+        # @api semipublic
+        def update(attributes, collection)
+          DataMapper.logger.debug("Update called with:\nAttributes #{attributes.inspect} \nCollection: #{collection.inspect}")
+          raise "Implement me!"
+        end
+        
+        # Deletes one or many existing resources
+        #
+        # @example
+        #   adapter.delete(collection)  # => 1
+        #
+        # Adapters provide specific implementation of this method
+        #
+        # @param [Collection] collection
+        #   collection of records to be deleted
+        #
+        # @return [Integer]
+        #   the number of records deleted
+        #
+        # @api semipublic
+        def delete(collection)
+          DataMapper.logger.debug("Delete called with: #{collection.inspect}")
+          raise "Implement me!"
+        end
+        
       end
     end
   end
