2c9c779f7cd2fb62c6ccaae270baea4393c55c14
Nass <nasali@anypresence.com>
2013-07-15 14:30:35 -0400

    Added more spec support.

--- lib/dm-dynamodb-adapter/adapter.rb
+++ lib/dm-dynamodb-adapter/adapter.rb
@@ -2,7 +2,14 @@ module DataMapper
   module Adapters
     module DynamoDB
       class Adapter < DataMapper::Adapters::AbstractAdapter
-        
+
+        def initialize(name, options)
+          super
+          access_key_id = @options.fetch(:aws_access_key_id)
+          secret_access_key = @options.fetch(:aws_secret_access_key)
+          @db = AWS::DynamoDB.new(:access_key_id => access_key_id,:secret_access_key => secret_access_key)
+          DataMapper::Logger.debug("Connected to #{@db}")
+        end
       end
     end
   end
--- spec/dm_dynamodb_adapter_spec.rb
+++ spec/dm_dynamodb_adapter_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe DataMapper::Adapters::DynamoDB::Adapter do
-  pending
+  before(:all) do
+    @adapter = DataMapper.setup(:default,
+        { :adapter  => :dynamodb,
+          :aws_access_key_id => TEST_ACCESS_KEY_ID,
+          :aws_secret_access_key => TEST_SECRET_ACCESS_KEY
+        }
+    )
+  end
   
 end
